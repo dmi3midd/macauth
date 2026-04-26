acc19032a114eda7b04849798428e9c911274286
dmi3mid <dmi3midd@gmail.com>
2026-04-27 00:50:20 +0300

    feat: initialize all layers and setup routes.

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -1,7 +1,11 @@
 package api
 
 import (
-	"net/http"
+	errs "macauth/internal/errors"
+	"macauth/internal/handlers"
+	"macauth/internal/middlewares"
+	"macauth/internal/repositories"
+	"macauth/internal/services"
 	"os"
 
 	"github.com/go-chi/chi/v5"
@@ -11,15 +15,32 @@ import (
 
 func (s *Server) RegisterRoutes() chi.Mux {
 	// env vars
-	_, ok := os.LookupEnv("API_KEY")
+	apiKey, ok := os.LookupEnv("API_KEY")
 	if !ok {
 		panic("API_KEY is required!")
 	}
 
-	// routers
+	// repositories
+	tokenRepo := repositories.NewTokenRepo(s.db.GetDB())
+	userRepo := repositories.NewUserRepo(s.db.GetDB())
+	clientRepo := repositories.NewClientRepo(s.db.GetDB())
+
+	// services
+	tokenService := services.NewTokenService(tokenRepo, s.cfg.Keys)
+	userService := services.NewUserService(userRepo, tokenService)
+	clientService := services.NewClientService(clientRepo)
+
+	// handlers
+	userHandler := handlers.NewUserHandler(userService)
+	clientHandler := handlers.NewClientHandler(clientService)
+
+	// middlewares
+	clientValidator := middlewares.NewClientValidator(clientRepo)
+	apikeyValidator := middlewares.NewApiKeyValidator(apiKey)
+
+	// routes
 	mainRouter := chi.NewRouter()
 
-	mainRouter.Use(middleware.Logger)
 	mainRouter.Use(cors.Handler(cors.Options{
 		AllowedOrigins:   []string{"https://*", "http://*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
@@ -27,9 +48,21 @@ func (s *Server) RegisterRoutes() chi.Mux {
 		AllowCredentials: true,
 		MaxAge:           300,
 	}))
+	mainRouter.Use(middleware.RequestID)
+	mainRouter.Use(middleware.Recoverer)
 
-	mainRouter.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
+	mainRouter.Group(func(r chi.Router) {
+		r.Use(apikeyValidator.Validate())
+		r.Post("/", errs.ErrorHandler(clientHandler.Link))
+		r.Delete("/{clientId}", errs.ErrorHandler(clientHandler.Unlink))
+	})
+
+	mainRouter.Group(func(r chi.Router) {
+		r.Use(clientValidator.Validate())
+		r.Post("/registration", errs.ErrorHandler(userHandler.Registration))
+		r.Post("/login", errs.ErrorHandler(userHandler.Login))
+		r.Delete("/logout", errs.ErrorHandler(userHandler.Logout))
+		r.Put("/refresh", errs.ErrorHandler(userHandler.Refresh))
 	})
 
 	return *mainRouter
